2e6b0888fce5e49c478f3288033cfecbd8d05979
zhuzl <zhuzl@ifeng.com>
2015-06-17 14:33:19 +0800

    And test

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class  Test{
 	public static void main(String [] args){
 		System.out.println("Hello,World");
 		System.out.println("This is a modified class created by me!");	
-		System.out.println("Test the new and branch!!!");
+		System.out.println("Test the new and^ branch!!!");
 	}
 }
