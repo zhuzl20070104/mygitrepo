ce67003f3e1b0675e06f3188661707b6cb022f8e
zhuzl20070104 <zhuzl@ifeng.com>
2015-06-17 15:00:34 +0800

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,7 +1,8 @@
 public class Test1{
   public static void main(String [] args){
     System.out.println("Hello,Test1!!!");
-    System.out.println("git  keng a!!!!");
+    System.out.println("git 大坑1 keng a!!!!");
+    System.out.println("大坑！");
    }
   }
 }
