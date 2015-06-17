549d3752b2abd05197c60d9eb2805f89b8e1cf2e
zhuzl <zhuzl@ifeng.com>
2015-06-17 15:47:19 +0800

    newddd

--- Test.java
+++ Test.java
@@ -3,7 +3,6 @@ public class  Test{
 		System.out.println("Hello,World");
 		System.out.println("This is a modified class created by and me!");	
 		System.out.println("Test the new and branch!!!");
-		System.out.println("新坑");
 		System.out.println("newnewkeng");
 		System.out.println("dadakeng");
 	}
