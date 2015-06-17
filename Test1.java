84e46e79c545f4a1754de4d6066ed9e2e0727dac
zhuzl <zhuzl@ifeng.com>
2015-06-17 15:04:38 +0800

    ttt

--- Test1.java
+++ Test1.java
@@ -1,7 +1,7 @@
 public class Test1{
   public static void main(String [] args){
     System.out.println("Hello,Test1!!!");
-    System.out.println("git  keng a!!!!");
+    System.out.println("git FFFDD$ keng a!!!!");
    }
   }
 }
